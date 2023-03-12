95404044a906b52578d46f48d461b5b3a7c0d32b
Lucas Fernandes <fernandeslucas451@outlook.com>
2023-03-12 18:48:06 -0300

    Removing Comments of Program.cs

--- cep-minimal-api/Program.cs
+++ cep-minimal-api/Program.cs
@@ -6,8 +6,6 @@ using System.Text.RegularExpressions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.ResolveDependencies();
